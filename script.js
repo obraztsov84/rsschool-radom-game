d9da548b4294aa30b0ea665d160b4aa874bf74b2
sergei.obraztsov <sergei.obraztsov@sshvps.net>
2022-02-21 02:46:42 +0300

    feat: working prototype ready

---
+++ script.js
@@ -0,0 +1,25 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+
+document.addEventListener("keydown", (e) => {
+  jump();
+})
+
+function jump () {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+  }
+  setTimeout(()=>{
+    dino.classList.remove("jump")
+  }, 400)
+}
+
+let isAlive = setInterval (() => {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >=140) {
+    console.log("Game over")
+  }
+}, 10)
